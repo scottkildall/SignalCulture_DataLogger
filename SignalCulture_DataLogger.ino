b1535beb54af81b05852e48597c809433ff7d674
Scott Kildall <scott@kildall.com>
2015-07-25 23:50:22 -0700

    Added RAM buffer for data logger

--- SignalCulture_DataLogger.ino
+++ SignalCulture_DataLogger.ino
@@ -8,9 +8,38 @@
   A momentary switch will begin logging
   Pressing the momentary switch will stop logging
   
-  We log to RAM and then flush when the buffer is filled.
+  Ideally, we would log to RAM and then flush when the buffer is filled.
   
   We are logging to microseconds, 1000 micros = 1 millis
+  
+  Other options would be to overclock, which might give us 2X the data-logging rate
+  However, this is complicated.
+  
+  Also, simply running this with a fan on it, too cool it down might make the CPU faster
+  
+  max # of characters
+  - signal value 0-1023 = 4 charcters
+  - command (,) = 1 character
+  - max unsigned long = 18446744073709551615 = 20
+  - newline = 1 chacter
+  - termination string = 1 char
+  - total = 27 characters, add 1 for safekeeping to make 28
+  
+  sample time on a is about 280 microseconds
+  a faster SD card will have marginal improvements since we are using a RAM buffer and flushing it every 20 lines or so
+  
+  Sample rate ~3500-3600 H
+  i.e. 3500-3600 samples/second
+  Pretty good!!
+  
+  ~ 1.7mb file for 60 seconds
+  60 minutes = 102mb
+  
+  The next issue is that we run out of micros(), so have an overflow problem after about a minute
+  We could fix this in code (best solution)
+  
+  Could also log for a specified amount of time, using a millis() timer
+  
  */
 
 #include <SD.h>
@@ -34,6 +63,12 @@ int analogPin = 0;
 File dataFile;
 
 unsigned long startMicros = 0;    // subtract from micros() when logging
+
+#define MAX_LINE_SIZE (28)
+#define BUFFER_SIZE (400)        // 400 is safe, 560 will cause error with opening datalog
+
+int bufferIndex = 0;
+char *buffer;
 
 void setup()
 {
@@ -64,7 +99,19 @@ void setup()
     return;
   }
   Serial.println("card initialized.");
+  Serial.print("buffer size = " );
+  Serial.println(BUFFER_SIZE);
   
+  // we allocate from SRAM (I think)
+  buffer = new char[BUFFER_SIZE];
+  
+  if( buffer == 0L ) {
+    Serial.println("new allocator failed" );
+    return;
+  }
+  
+  Serial.print("buffer address = " );
+  Serial.println( (unsigned long)buffer );
   // red on
   bSwitchPressed = false;
   bLogging = false;
@@ -97,10 +144,17 @@ void loop()
     bSwitchPressed = false;
   }
   if( bLogging ) {
-    // we will want to optimize this to write to a buffer and then flush the buffer every few c
-    dataFile.print(analogRead(analogPin));  
-    dataFile.print(",");
-    dataFile.println(micros() - startMicros);
+    // we will want to optimize this to write to a buffer and then flush the buffer when we fill it up
+    sprintf(buffer + bufferIndex, "%d,%lu\n", analogRead(analogPin),micros() - startMicros);
+    bufferIndex += strlen(buffer+bufferIndex);
+    
+    // check to see if we are about to overflow the vuffer
+    if( bufferIndex > BUFFER_SIZE - MAX_LINE_SIZE ) {
+      bufferIndex = 0;
+ 
+      // buffer of data
+      dataFile.print(buffer+bufferIndex);  // see if the entire thing prints out
+    }
   }
 }
 
@@ -131,6 +185,7 @@ void beginLogging() {
     dataFile.println("value, ms");
     
      bLogging = true;
+     bufferIndex = 0;
      
      startMicros = micros();
   }  
@@ -142,6 +197,10 @@ void beginLogging() {
 }
 
 void endLogging() {
+  // log the remaining buffer
+  bufferIndex = 0;
+  dataFile.print(buffer+bufferIndex);  // see if the entire thing prints out
+      
   dataFile.close();
   bLogging = false;
   digitalWrite(greenLED, LOW);
@@ -149,16 +208,10 @@ void endLogging() {
 }
 
 
-
-
-
 //-- interrupt routine, set flag
 void switchPressed() {
   bSwitchPressed = true;
 }
-
-
-
 
 
 
